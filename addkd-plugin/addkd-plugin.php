fffc7ab93bb94e06544db7057377f7d8f2c728d7
kirandash <thekirankumardash@gmail.com>
2017-08-27 16:45:52 +0530

    1 WP Plugin create folder and header requirements

---
+++ addkd-plugin/addkd-plugin.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+* @package Addkd Plugin
+*/
+/*
+Plugin Name: Addkd Plugin
+Plugin URI: http://www.bgwebagency.com
+Description: This is a custom WP plugin.
+Version: 1.0.0
+Author: Kiran Kumar Dash
+Author URI: http://www.kirandash.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: addkd-plugin
+*/
+?>
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Plugin Development Intro
+
+Turn WP_DEBUG true
+
+Check unique plugin name
+
+Plugin - file name should match the folder name. 
