397f9d727ee905a4b5da4c8a66d9d5c9a857d878
kirandash <thekirankumardash@gmail.com>
2017-08-27 21:48:28 +0530

    B. 9 Create table with dbDelta

--- tlg-recipe/includes/activate.php
+++ tlg-recipe/includes/activate.php
@@ -4,5 +4,20 @@ function tlgr_activate_plugin() {
 		// If wp version is less than 4.2 than plugin won't be activated
 		wp_die( __('You must update your WP installation to 4.8 or higher to use this plugin', 'tlg-recipe') );
 	}
+
+	// Create Table for ratings in db while activation
+	global $wpdb;
+	$createSQL = "
+		CREATE TABLE `". $wpdb->prefix ."recipe_ratings` (
+		  `id` bigint(20) NOT NULL,
+		  `recipe_id` bigint(20) NOT NULL,
+		  `rating` float NOT NULL,
+		  `user_ip` varchar(32) NOT NULL
+		) ENGINE=InnoDB ". $wpdb->get_charset_collate() .";
+		";
+
+	require( ABSPATH . '/wp-admin/includes/upgrade.php' ); // to include dbDelta fn which allows us to modify the wp database
+	dbDelta( $createSQL );
+	// use exit(); at end of dbDelta fn in /wp-admin/includes/upgrade.php for debugging
 }
 ?>
