4168d3f8376c485ec6f7f12e974622b9880c68f3
kirandash <thekirankumardash@gmail.com>
2017-09-17 11:37:53 +0530

    014 Handling AJAX Requests and Inserting Data into the Database

--- tlg-recipe/process/rate-recipe.php
+++ tlg-recipe/process/rate-recipe.php
@@ -1,5 +1,35 @@
 <?php 
 function tlgr_rate_recipe() {
-	print_r($_POST);
-	die();
+	/*print_r($_POST);
+	die();*/
+	global $wpdb;
+
+	$response		= array( 'status'	=> 1 ); // failure status
+	$post_id 		= absint($_POST['rid']);
+	$rating		 	= round($_POST['rating'], 1);
+	$user_ip		= $_SERVER['REMOTE_ADDR']; // get user's ip. This is optional but used for security purposes
+
+	$rating_count = $wpdb->get_var("SELECT COUNT(*) FROM `" . $wpdb->prefix . "recipe_ratings` WHERE recipe_id='". $post_id ."' AND user_ip='".$user_ip."'");
+
+	if($rating_count > 0){
+		wp_send_json($response);
+	} // Insert rating only once
+
+	$wpdb->insert(
+		$wpdb->prefix . 'recipe_ratings', // table name
+		array(
+			'recipe_id'	=>	$post_id,
+			'rating'	=>  $rating,
+			'user_ip'	=>	$user_ip
+		), // values
+		array(
+			'%d', '%f', '%s'
+		)  // formats ( %s string, %d integer, %f float )
+	);
+
+	$response['status']		= 2; // success status 
+	// echo $response['status'];
+
+	// die();
+	wp_send_json( $response );
 }
