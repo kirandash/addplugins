91ef6acc71c8b270e0796627fbf0b78856768842
kirandash <thekirankumardash@gmail.com>
2017-10-02 23:14:28 +0530

    A 3 WP Plugin - Basic PHP OOP

--- addkd-plugin/addkd-plugin.php
+++ addkd-plugin/addkd-plugin.php
@@ -23,4 +23,55 @@ endif;
 if( ! function_exists( 'add_action' ) ) {
 	exit;
 }
+
+class AddkdPlugin
+{
+	/*function __construct(string $arg) { // construct will run when class is initialized and through this we can pass some arguments to our class - like procedural code for funciton calling
+		echo $arg;
+	}*/
+	/*function __construct() { // construct will run when class is initialized and through this we can pass some arguments to our class - like procedural code for funciton calling
+		
+	}
+	// methods
+	function method1(){
+
+	}
+
+	function method2(){
+
+	}*/
+	function activate(){ // No need for unique names since this method is not publicly accessible
+		// echo 'The plugin was activated successfully'; // This will create header already sent error
+		// generate a CPT
+		// flush rewrite rules
+	}
+	function deactivate(){
+		// echo 'The plugin was deactivated successfully'; 
+		// flush rewrite rules
+	}
+	function uninstall(){
+		// delete CPT
+		// delete all the plugin data from the DB
+	}
+}
+
+if( class_exists( 'AddkdPlugin' ) ) { // Security checkup to see if a class exists
+	// $addkdPlugin = new AddkdPlugin( 'Addkd Plugins initialized!' ); // Store the initialization in a variable to use its instance in different places
+	$addkdPlugin = new AddkdPlugin(  );
+}
+
+/*function customFunction($arg) {
+	echo $arg;
+}
+
+customFunction('this is the argument to echo'); // Procedural code / functional programming in PHP*/
+
+// On activation
+register_activation_hook( __FILE__ , array($addkdPlugin, 'activate')); // instead of function name we are accessing a fn from a class thus an array with instance of that class and then the function name is passed
+
+// On deactivation
+register_deactivation_hook( __FILE__ , array($addkdPlugin, 'deactivate')); 
+
+// On uninstall
+
 ?>
